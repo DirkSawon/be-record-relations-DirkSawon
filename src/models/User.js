744a0e29cbb87b2589ab79f030fc652adb0eb738
DirkSawon <Dirk.Sawon@gmx.de>
2023-09-06 13:18:19 +0200

    Version 0.01 - Setting up Project

---
+++ src/models/User.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose');
+
+const schema = new Schema({
+    id: Number,
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String
+}, { timestamps: true});
+
+const userModel = new model('User', schema, 'users');
+
+module.exports = userModel;
